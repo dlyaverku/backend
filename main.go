0398febaf22e1f72862ab44530e68678efc4139e
Dlyaverku <dlyaverku@gmail.com>
2024-12-21 17:53:39 +0700

    update

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	_ "backend/docs" // Подключаем сгенерированные Swagger-документы
 	"backend/handlers"
 
+	"github.com/gin-contrib/cors" // Подключаем библиотеку для работы с CORS
 	"github.com/gin-gonic/gin"
 
 	swaggerFiles "github.com/swaggo/files"
@@ -13,10 +14,20 @@ import (
 // @title User API
 // @version 1.0
 // @description API для работы с пользователями.
-// @host localhost:8080
+// @host localhost:8081
 // @BasePath /
 func main() {
+	// Создаем новый роутер
 	router := gin.Default()
+
+	// Настройка CORS middleware
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://5.35.83.98/"},                      // Разрешаем запросы с фронтенда на localhost:3000
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},            // Разрешаем методы
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"}, // Разрешаем заголовки
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true, // Разрешаем использование cookies и заголовков авторизации
+	}))
 
 	// Маршруты API
 	router.GET("/users", handlers.GetUsers)
@@ -26,5 +37,5 @@ func main() {
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	// Запуск сервера
-	router.Run(":8080")
+	router.Run(":8081")
 }
